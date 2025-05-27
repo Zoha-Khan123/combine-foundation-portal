489b1c962f825f20f5b8ebc2f2e07ebc045ae550
Zoha-Khan123 <zohakhan11004@gmail.com>
2025-05-27 10:08:39 +0500

    vercel link add in backend

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://full-stack-project-id5k.vercel.app","http://localhost:3000"],  
+    allow_origins=["https://combine-foundation-portal.vercel.app"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
